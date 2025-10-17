82c6ca9fa6955d96181b44f0ffa143a430f0fb7a
Bas Maas <github.hunter841@passmail.net>
2025-10-17 11:23:26 +0200

    Removed base

--- base/apps/external-dns/README.md
+++
@@ -1,3 +0,0 @@
-# External DNS
-
-Followed this guide: [ExternalDNS with Pi-hole](https://kubernetes-sigs.github.io/external-dns/v0.15.0/docs/tutorials/pihole/#externaldns-manifest)
--- base/infra/files.tf
+++
@@ -1,8 +0,0 @@
-resource "proxmox_virtual_environment_download_file" "talos_image" {
-  content_type = var.talos_image.content_type
-  datastore_id = var.talos_image.datastore_id
-  file_name    = var.talos_image.file_name
-  node_name    = var.talos_image.node_name
-  url          = var.talos_image.url
-  overwrite    = var.talos_image.overwrite
-}
